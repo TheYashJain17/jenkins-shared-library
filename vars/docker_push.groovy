72dd1204294c57cf1b8a6fcad759de2ee757acb6
Yash Jain <yashjain07.yj@gmail.com>
2025-11-12 18:45:50 +0530

    Made Some changes in docker_push.groovy to make it dynamic

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,4 +1,4 @@
-def call(){
+def call(String projectName, String imageTag){
 
    withCredentials([
                     usernamePassword(
@@ -9,8 +9,8 @@ def call(){
                     ]){
                         
                         sh "export PATH=$PATH:/usr/local/bin && docker login -u ${env.dockerUser} -p ${dockerPass}"
-                        sh "export PATH=$PATH:/usr/local/bin && docker image tag ai-landing-page ${env.dockerUser}/ai-landing-page"
-                        sh "export PATH=$PATH:/usr/local/bin && docker push ${env.dockerUser}/ai-landing-page"
+                        sh "export PATH=$PATH:/usr/local/bin && docker image tag ${env.dockerUser}/${projectName}:${imageTag} ${env.dockerUser}/${projectName}:${imageTag}"
+                        sh "export PATH=$PATH:/usr/local/bin && docker push ${env.dockerUser}/${projectName}:${imageTag}"
                         
                     }
                     
